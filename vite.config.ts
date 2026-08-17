6d70d64dcb272c548b5fe567b0fed52a458c20e7
2022victtorsilva-debug <2022victtorsilva@gmail.com>
2026-08-16 21:06:51 -0300

    Configura Vite sem dependências do Lovable

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
+import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { nitro } from "nitro/vite";
+import tsConfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [tsConfigPaths(), tailwindcss(), tanstackStart(), nitro(), viteReact()],
+});
